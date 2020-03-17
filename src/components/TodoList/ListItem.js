cba35b3ed7bdfae3aa3484ea5557fc710a39b6ac
Anton Melnikov <antonmelnikov@Antons-MacBook-Pro.local>
2020-03-17 14:49:24 +0300

    TodoList: refactoring (functional component) and propTypes added

--- src/components/TodoList/ListItem.js
+++ src/components/TodoList/ListItem.js
@@ -1,78 +1,89 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './ListItem.css';
 import { FaCheck, FaTrash, FaPen } from 'react-icons/fa';
+import PropTypes from 'prop-types';
 
-export default class ListItem extends Component {
 
-  render() {
-    const date = new Date(this.props.data.created_at).toLocaleString();
-    return(
-      <div
-        className={this.props.data.is_completed ? 'LIContainer completed':'LIContainer active'}
-      >
-        <div className="LIHeader">
-          <span className="LIHead">{this.props.data.task}</span>
-          <span className="LIInfo">
-            <span className="LIDate"><p>{date}</p></span>
-            <span className="LIControls">
-              <span
-                className="LIToggle"
-                title="Toggle completed"
-                onClick={() => {
-                  if (typeof this.props.onToggle === 'function') {
-                    this.props.onToggle(this.props.data.id, !this.props.data.is_completed);
-                  } else {
-                    alert('Action is not provided!');
-                  }
-                }}
-              >
-                <FaCheck size={22} />
-              </span>
-              <span
-                className="LIEdit"
-                title="Edit"
-                onClick={() => {
-                  if (typeof this.props.onEdit === 'function') {
-                    const todoToEdit = {
-                      id: this.props.data.id,
-                      task: this.props.data.task,
-                      description: this.props.data.description
-                    }
-                    this.props.onEdit(todoToEdit);
-                  } else {
-                    alert('Action is not provided!');
-                  }
-                }}
-              >
-                <FaPen size={22} />
-              </span>
-              <span
-                className="LIDelete"
-                title="Delete"
-                onClick={() => {
-                  if (typeof this.props.onDelete === 'function') {
-                    if (window.confirm(`Are you sure, you want to delete task: ${this.props.data.task}?`)) {
-                      this.props.onDelete(this.props.data.id);
-                    }
+const ListItem = (props) => {
 
-                  } else {
-                    alert('Action is not provided!');
+  const date = new Date(props.data.created_at).toLocaleString();
+  return(
+    <div
+      className={props.data.is_completed ? 'LIContainer completed':'LIContainer active'}
+    >
+      <div className="LIHeader">
+        <span className="LIHead">{props.data.task}</span>
+        <span className="LIInfo">
+          <span className="LIDate"><p>{date}</p></span>
+          <span className="LIControls">
+            <span
+              className="LIToggle"
+              title="Toggle completed"
+              onClick={() => {
+                if (typeof props.onToggle === 'function') {
+                  props.onToggle(this.props.data.id, !props.data.is_completed);
+                } else {
+                  alert('Action is not provided!');
+                }
+              }}
+            >
+              <FaCheck size={22} />
+            </span>
+            <span
+              className="LIEdit"
+              title="Edit"
+              onClick={() => {
+                if (typeof props.onEdit === 'function') {
+                  const todoToEdit = {
+                    id: props.data.id,
+                    task: props.data.task,
+                    description: props.data.description
                   }
-                }}
-              >
-                <FaTrash size={22} />
-              </span>
+                  props.onEdit(todoToEdit);
+                } else {
+                  alert('Action is not provided!');
+                }
+              }}
+            >
+              <FaPen size={22} />
+            </span>
+            <span
+              className="LIDelete"
+              title="Delete"
+              onClick={() => {
+                if (typeof props.onDelete === 'function') {
+                  if (window.confirm(`Are you sure, you want to delete task: ${props.data.task}?`)) {
+                    props.onDelete(props.data.id);
+                  }
+                } else {
+                  alert('Action is not provided!');
+                }
+              }}
+            >
+              <FaTrash size={22} />
             </span>
           </span>
-        </div>
-        {this.props.data.description === '' ? null :
-        <div className="LIText">
-          <p>{this.props.data.description}</p>
-        </div>
-        }
-
-
+        </span>
       </div>
-    );
-  }
+      {props.data.description === '' ? null :
+      <div className="LIText">
+        <p>{props.data.description}</p>
+      </div>
+      }
+    </div>
+  );
 }
+
+ListItem.propTypes = {
+  data: PropTypes.shape({
+    task: PropTypes.string.isRequired,
+    description: PropTypes.string,
+    created_at: PropTypes.number.isRequired,
+    is_completed: PropTypes.bool.isRequired
+  }).isRequired,
+  onToggle: PropTypes.func,
+  onEdit: PropTypes.func,
+  onDelete: PropTypes.func
+};
+
+export default ListItem;
--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -1,65 +1,77 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ListItem from './ListItem';
 import './TodoList.css';
+import PropTypes from 'prop-types';
 
 
-export default class TodoList extends Component {
+const TodoList = (props) => {
 
-  static defaultProps = {
-    sorted: 'desc',
-    filtered: 'all',
-    search: ''
+  let filteredTodos = props.data;
+  if (props.sorted) {
+    filteredTodos.sort((a, b) => {
+      if (props.sorted === 'asc') return a.created_at - b.created_at;
+      if (props.sorted === 'desc') return b.created_at - a.created_at;
+      return null;
+    });
   }
+  if (props.filtered && props.filtered !== 'all') {
+    filteredTodos = filteredTodos.filter((todo) => {
+      if (props.filtered === 'completed' && todo.is_completed) return true;
+      if (props.filtered === 'active' && !todo.is_completed) return true;
+      return false;
+    })
+  }
+  if (props.search && props.search !== '') {
+    filteredTodos = filteredTodos.filter((todo) => {
+      return todo.task.toLowerCase().search(props.search.toLowerCase()) !== -1
+          || todo.description.toLowerCase().search(props.search.toLowerCase()) !== -1;
+    });
+  }
+  const listItems = filteredTodos.map((item) =>
+        <ListItem
+          key={item.id}
+          data={item}
+          onToggle={props.toggle}
+          onEdit={props.edit}
+          onDelete={props.delete}
+        />
+  );
 
-  render() {
-    let filteredTodos = this.props.data;
-    if (this.props.sorted) {
-      filteredTodos.sort((a, b) => {
-        if (this.props.sorted === 'asc') return a.created_at - b.created_at;
-        if (this.props.sorted === 'desc') return b.created_at - a.created_at;
-        return null;
-      });
-    }
-    if (this.props.filtered && this.props.filtered !== 'all') {
-      filteredTodos = filteredTodos.filter((todo) => {
-        if (this.props.filtered === 'completed' && todo.is_completed) return true;
-        if (this.props.filtered === 'active' && !todo.is_completed) return true;
-        return false;
-      })
-    }
-    if (this.props.search && this.props.search !== '') {
-      filteredTodos = filteredTodos.filter((todo) => {
-        return todo.task.toLowerCase().search(this.props.search.toLowerCase()) !== -1
-            || todo.description.toLowerCase().search(this.props.search.toLowerCase()) !== -1;
-      });
-    }
-    const listItems = filteredTodos.map((item) =>
-          <ListItem
-            key={item.id}
-            data={item}
-            onToggle={this.props.toggle}
-            onEdit={this.props.edit}
-            onDelete={this.props.delete}
-          />
-    );
-
-    return(
-      <div className="TDLContainer">
-        <span className="TDLHeader">
-          <span className="TDLTitle">
-            Todo List
-          </span>
-          <span className="TDLLegend">
-              <span className="square active"></span>
-              <span className="descText"> - active</span>
-              <span className="square completed"></span>
-              <span className="descText"> - completed</span>
-          </span>
-
+  return(
+    <div className="TDLContainer">
+      <span className="TDLHeader">
+        <span className="TDLTitle">
+          Todo List
         </span>
-
-          {listItems}
-      </div>
-    );
-  }
+        <span className="TDLLegend">
+            <span className="square active"></span>
+            <span className="descText"> - active</span>
+            <span className="square completed"></span>
+            <span className="descText"> - completed</span>
+        </span>
+      </span>
+        {listItems}
+    </div>
+  );
 }
+
+TodoList.defaultProps = {
+  sorted: 'desc',
+  filtered: 'all',
+  search: '',
+  toggle: () => {console.warn('Toggle action is not provided')},
+  edit: () => {console.warn('Edit action is not provided')},
+  delete: () => {console.warn('Delete action is not provided')},
+};
+
+TodoList.propTypes = {
+  data: PropTypes.arrayOf(PropTypes.object).isRequired,
+  filtered: PropTypes.oneOf(['all', 'completed', 'active']),
+  sorted: PropTypes.oneOf(['desc', 'asc']),
+  search: PropTypes.string,
+  toggle: PropTypes.func,
+  edit: PropTypes.func,
+  delete: PropTypes.func,
+};
+
+export default TodoList;
